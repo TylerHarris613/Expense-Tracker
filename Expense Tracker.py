71665f32a37dacc23c0c9ca77b50dda9e1bc0d2a
Tyler Harris <tylerharris@MacBook-Air.local>
2024-10-29 21:06:42 -0400

    Decided on program flow. Create all functions. Finished the function to receive user input

--- Expense Tracker.py
+++ Expense Tracker.py
@@ -3,3 +3,27 @@
 # View all expenses.
 # Calculate the total amount spent.
 
+# To push updates:
+# git add .
+# git commit -m "Description of changes made"
+# git push
+
+from decimal import Decimal
+
+# This function allows users to input expenses with a description and a dollar amount
+def expenseGet():
+    try:    
+        expAmount = Decimal(input("How much is the expense?")) # Using Decimal() due to the money nature of the input. You should pass strings to Decimal in the User input
+        print(f"You submitted an expense of ${expAmount}")    
+    except ValueError:
+        print("You're entering a money value here dummy, so make it a number when inputting!!!!\n")
+    expDisc = input("What is the expense for?")
+    print(f"You submitted an expense with description {expDisc}")
+    return {expDisc:expAmount}
+
+def expenseAdd(expenseDisc):
+
+def expenseView():
+
+
+def totalExpenseCalc():
